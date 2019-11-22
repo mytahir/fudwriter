89e85c958d6ec0f8c5661f0b355bd98ebd18cca1
Mustapha Yusuf <musty4000@gmail.com>
2019-11-22 01:45:49 +0000

    Reduce left Animation

---
+++ fudwriter/fudwriter/LeftMenu.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace fudwriter
+{
+    /// <summary>
+    /// Interaction logic for LeftMenu.xaml
+    /// </summary>
+    public partial class LeftMenu : UserControl
+    {
+        public LeftMenu()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
