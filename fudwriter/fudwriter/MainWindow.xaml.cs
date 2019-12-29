626c854a952d4a4e297fd11c67b6562e9fa0dedc
Mustapha Yusuf <musty4000@gmail.com>
2019-12-29 04:16:39 +0000

    Add resizable canvas

--- fudwriter/fudwriter/MainWindow.xaml.cs
+++ fudwriter/fudwriter/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using Fluent;
-
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 
 namespace fudwriter
 {
@@ -22,6 +25,47 @@ namespace fudwriter
 
         }
 
+        void onDragDelta(object sender, DragDeltaEventArgs e)
+        {
+            //Move the Thumb to the mouse position during the drag operation
+            double yadjust = myCanvasStretch.Height + e.VerticalChange;
+            double xadjust = myCanvasStretch.Width + e.HorizontalChange;
+            if ((xadjust >= 0) && (yadjust >= 0))
+            {
+                myCanvasStretch.Width = xadjust;
+                myCanvasStretch.Height = yadjust;
+                Canvas.SetLeft(myThumb, Canvas.GetLeft(myThumb) +
+                                        e.HorizontalChange);
+                Canvas.SetTop(myThumb, Canvas.GetTop(myThumb) +
+                                        e.VerticalChange);
+                //changes.Text = "Size: " +
+                //                myCanvasStretch.Width.ToString() +
+                //                 ", " +
+                //                myCanvasStretch.Height.ToString();
+            }
+        }
+
+        private void myThumb_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Cursor cur = Cursors.SizeNWSE;
+            this.Cursor = cur;
+        }
+
+        private void myThumb_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            Cursor cur = Cursors.Arrow;
+            this.Cursor = cur;
+        }
+
+        void onDragStarted(object sender, DragStartedEventArgs e)
+        {
+            myThumb.Background = Brushes.Orange;
+        }
+
+        void onDragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            myThumb.Background = Brushes.Blue;
+        }
         //private void FileTab_Click(object sender, RoutedEventArgs e)
         //{
         //    int index = int.Parse(((MenuItem)e.Source).Uid);
